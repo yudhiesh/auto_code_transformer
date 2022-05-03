7daf5b597b4c467fad54f4b576fe066f1b95d193
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2022-05-03 14:08:54 +0800

    refactor test methods

--- tests/test_auto_code_transformer.py
+++ tests/test_auto_code_transformer.py
@@ -8,6 +8,34 @@ from src.process_file import ProcessPythonFileBase
 from src.transformer import AssignmentValueTransformer
 
 
+def get_stub_process_python_file(expected):
+    class StubProcessPythonFile(ProcessPythonFileBase):
+        def __init__(self, file_path: str) -> None:
+            super().__init__(file_path)
+            self._red_baron_object: Optional[RedBaron] = None
+
+        @property
+        def red_baron_object(self) -> Optional[RedBaron]:
+            return super().red_baron_object
+
+        def read_file(self) -> None:
+            return super().read_file()
+
+        def write_file(self, code_object: RedBaron) -> None:
+            assert str(code_object.assignment.value) == str(expected.assignment.value)
+
+    return StubProcessPythonFile
+
+
+def check_exception(expected, func):
+    if type(expected) == type and issubclass(expected, Exception):
+        # Verify
+        with pytest.raises(expected):
+            func()
+    else:
+        func()
+
+
 @pytest.mark.parametrize(
     "values,file_path,expected",
     [
@@ -29,20 +57,7 @@ from src.transformer import AssignmentValueTransformer
     ],
 )
 def test_assignment_value_transformer(values, file_path, expected):
-    class StubProcessPythonFile(ProcessPythonFileBase):
-        def __init__(self, file_path: str) -> None:
-            super().__init__(file_path)
-            self._red_baron_object: Optional[RedBaron] = None
-
-        @property
-        def red_baron_object(self) -> Optional[RedBaron]:
-            return super().red_baron_object
-
-        def read_file(self) -> None:
-            return super().read_file()
-
-        def write_file(self, code_object: RedBaron) -> None:
-            assert str(code_object.assignment.value) == str(expected.assignment.value)
+    StubProcessPythonFile = get_stub_process_python_file(expected=expected)
 
     # Setup
     py_file_processor = StubProcessPythonFile(file_path=file_path)
@@ -50,10 +65,4 @@ def test_assignment_value_transformer(values, file_path, expected):
         processor=py_file_processor,
         values=values,
     )
-    # Exercise
-    if type(expected) == type and issubclass(expected, Exception):
-        # Verify
-        with pytest.raises(expected):
-            assignment_value_transformer.run()
-    else:
-        assignment_value_transformer.run()
+    check_exception(expected, assignment_value_transformer.run)
